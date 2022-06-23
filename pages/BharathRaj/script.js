8e1a672feb88096b4b28dd73239b2f5f5023380a
BharathRajVamsi <107875902+BharathRajVamsi@users.noreply.github.com>
2022-06-23 21:05:44 +0530

    updated_script_JS_index.html

--- pages/BharathRaj/script.js
+++ pages/BharathRaj/script.js
@@ -1,4 +1,4 @@
-function myprofile() {
+function myFunction() {
     let password = prompt("Please enter your password : 2425");
     if(password == 8688){
         window.open('/pages/BharathRaj/index.html')
