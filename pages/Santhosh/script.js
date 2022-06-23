92b530849de9f0a0bf7ae112c32f591e48c7d067
santocogent <santhosh.prabha@cogentibs.in>
2022-06-23 16:06:28 +0530

    update

---
+++ pages/Santhosh/script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+ 
+    var password = prompt("Enter in the password");
+    if (password=="santo") {
+        location = "https://darling-kulfi-8e9723.netlify.app/"
+      
+    }
+    else 
+    {  
+      location = "index.html"
+    }
+}
